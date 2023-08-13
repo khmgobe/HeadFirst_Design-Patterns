aef4120e116525832793e030e946b897d06a86d7
khm <dr94406@naver.com>
2023-08-13 23:44:29 +0900

    [refactor] 스윙 실습 부분 람다식으로 변경.

--- src/designPattern/ObserverPattern/SwingObserverExample.java
+++ src/designPattern/ObserverPattern/SwingObserverExample.java
@@ -15,20 +15,11 @@ public class SwingObserverExample {
         frame = new JFrame();
 
         JButton button = new JButton("할까? 말까?");
-        button.addActionListener(new AngelListener());
-        button.addActionListener(new DevilListener());
-    }
+        button.addActionListener(event ->
+                System.out.println("하지 마 아마 후회 할 걸?"));
 
-    class AngelListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("하지 마! 아마 후회 할 걸?");
-        }
-    }
+        button.addActionListener(event ->
+                System.out.println("그냥 저질러 버렷!!!"));
 
-    class DevilListener implements ActionListener {
-
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("그냥 저질러 버렷!!!");
-        }
     }
 }
