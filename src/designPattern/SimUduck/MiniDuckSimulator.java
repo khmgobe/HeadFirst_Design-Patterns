cd96dc38c120f0175b2ca98060a5c330cd32d4ed
khm <dr94406@naver.com>
2023-08-13 20:16:41 +0900

    [feat] 동적으로 행동 지정하기 이론 및 실습 완료.

--- src/designPattern/SimUduck/MiniDuckSimulator.java
+++ src/designPattern/SimUduck/MiniDuckSimulator.java
@@ -2,8 +2,9 @@ package designPattern.SimUduck;
 
 public class MiniDuckSimulator {
     public static void main(String[] args) {
-        Duck mallard = new MallardDuck();
-        mallard.performQuack();
-        mallard.performFly();
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
